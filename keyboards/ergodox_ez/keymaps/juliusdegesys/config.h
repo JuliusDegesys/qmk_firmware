bae4f06ebefe181a7fc6a650e7f0714fa45cae2b
Julius Degesys <julius@gauntlet.network>
2023-01-15 10:41:29 -0500

    Add 34-key user keymap for Ergodox EZ

---
+++ keyboards/ergodox_ez/keymaps/juliusdegesys/config.h
@@ -0,0 +1,37 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#pragma once
+
+#define ORYX_CONFIGURATOR
+#define IGNORE_MOD_TAP_INTERRUPT
+#undef TAPPING_TERM
+#define TAPPING_TERM 365
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"P7QPL/xyjeY"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
+
+#define LAYOUT_ez34(                                                \
+    K00, K01, K02, K03, K04,               K05, K06, K07, K08, K09, \
+    K10, K11, K12, K13, K14,               K15, K16, K17, K18, K19, \
+    K20, K21, K22, K23, K24,               K25, K26, K27, K28, K29, \
+                   K30, K31,               K32, K33                 \
+)                                                                   \
+LAYOUT_ergodox_pretty(                                              \
+    _______, _______, _______, _______, _______ , _______, QK_BOOT,      _______, _______, _______, _______, _______, _______, _______, \
+    _______, K00    , K01    , K02    , K03     , K04    , _______,      _______, K05    , K06    , K07    , K08    , K09    , _______, \
+    _______, K10    , K11    , K12    , K13     , K14    ,                        K15    , K16    , K17    , K18    , K19    , _______, \
+    _______, K20    , K21    , K22    , K23     , K24    , _______,      _______, K25    , K26    , K27    , K28    , K29    , _______, \
+    _______, _______, _______, _______, K30     ,                                          K33    , _______, _______, _______, _______, \
+                                                  _______, _______,      _______, _______,                                              \
+                                                           _______,      _______,                                                       \
+                                        K31     , _______, _______,      _______, _______, K32                                          \
+)
+
---
+++ keyboards/ergodox_ez/keymaps/juliusdegesys/keymap.c
@@ -0,0 +1,330 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+
+#define KC_MAC_UNDO LGUI(KC_Z)
+#define KC_MAC_CUT LGUI(KC_X)
+#define KC_MAC_COPY LGUI(KC_C)
+#define KC_MAC_PASTE LGUI(KC_V)
+#define KC_PC_UNDO LCTL(KC_Z)
+#define KC_PC_CUT LCTL(KC_X)
+#define KC_PC_COPY LCTL(KC_C)
+#define KC_PC_PASTE LCTL(KC_V)
+#define ES_LESS_MAC KC_GRAVE
+#define ES_GRTR_MAC LSFT(KC_GRAVE)
+#define ES_BSLS_MAC ALGR(KC_6)
+#define NO_PIPE_ALT KC_GRAVE
+#define NO_BSLS_ALT KC_EQUAL
+#define LSA_T(kc) MT(MOD_LSFT | MOD_LALT, kc)
+#define BP_NDSH_MAC ALGR(KC_8)
+#define SE_SECT_MAC ALGR(KC_6)
+
+#define LA_SYM MO(SYM)
+#define LA_NAV MO(NAV)
+
+enum custom_keycodes {
+    // Custom oneshot mod implementation with no timers.
+    OS_SHFT = EZ_SAFE_RANGE,
+    OS_CTRL,
+    OS_ALT,
+    OS_CMD,
+    OS_MEH,
+    OS_HYPR,
+
+    SW_WIN,  // Switch to next window         (cmd-tab)
+    SW_LANG, // Switch to next input language (ctl-spc)
+};
+
+enum layers {
+    DEF,
+    SYM,
+    NAV,
+    NUM,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [DEF] = LAYOUT_ez34(
+    KC_QUOT, KC_COMM, KC_DOT, KC_P  , KC_Y   , KC_F  , KC_G   , KC_C, KC_R, KC_L,
+    KC_A   , KC_O   , KC_E  , KC_U  , KC_I   , KC_D  , KC_H   , KC_T, KC_N, KC_S,
+    KC_SCLN, KC_Q   , KC_J  , KC_K  , KC_X   , KC_B  , KC_M   , KC_W, KC_V, KC_Z,
+                              LA_NAV, KC_LSFT, KC_SPC, LA_SYM
+  ),
+  [SYM] = LAYOUT_ez34(
+    KC_TILD, KC_UNDS, KC_PLUS, KC_QUES, KC_PIPE, XXXXXXX, KC_EXLM, KC_AT  , KC_HASH, KC_DLR ,
+    XXXXXXX, KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, XXXXXXX, KC_RPRN, KC_RBRC, KC_RCBR, KC_ESC ,
+    KC_GRV , KC_MINS, KC_EQL , KC_SLSH, KC_BSLS, XXXXXXX, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR,
+                               _______, _______, _______, _______
+  ),
+  [NAV] = LAYOUT_ez34(
+    XXXXXXX    , SW_WIN    , LGUI(LSFT(KC_T)), LGUI(KC_T)  , LGUI(KC_SPACE)  , KC_PGUP, LCTL(LSFT(KC_TAB)), KC_UP  , LCTL(KC_TAB), KC_ESC ,
+    OS_SHFT    , OS_CTRL   , OS_ALT          , OS_CMD      , OS_HYPR         , KC_ENT , KC_LEFT           , KC_DOWN, KC_RGHT     , KC_BSPC,
+    KC_MAC_UNDO, KC_MAC_CUT, KC_MAC_COPY     , KC_MAC_PASTE, LGUI(LSFT(KC_4)), KC_PGDN, LSFT(KC_TAB)      , KC_TAB , KC_GRAVE    , KC_DEL ,
+                                                    _______      , _______         , _______                    , _______
+  ),
+  [NUM] = LAYOUT_ez34(
+    KC_F1 , KC_F2 , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,
+    KC_1  , KC_2  , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,
+    KC_F11, KC_F12, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                             _______, _______, _______, _______
+  ),
+};
+
+
+
+extern rgb_config_t rgb_matrix_config;
+
+void keyboard_post_init_user(void) {
+  rgb_matrix_enable();
+}
+
+const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
+    [DEF] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {131,255,238}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
+    [NAV] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {0,0,0}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {216,218,204}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
+    [SYM] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,0,0}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,0,0}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,0,0}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
+    [NUM] = { {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {74,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {74,255,255}, {74,255,255}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0} },
+
+};
+
+void set_layer_color(int layer) {
+  for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+    HSV hsv = {
+      .h = pgm_read_byte(&ledmap[layer][i][0]),
+      .s = pgm_read_byte(&ledmap[layer][i][1]),
+      .v = pgm_read_byte(&ledmap[layer][i][2]),
+    };
+    if (!hsv.h && !hsv.s && !hsv.v) {
+        rgb_matrix_set_color( i, 0, 0, 0 );
+    } else {
+        RGB rgb = hsv_to_rgb( hsv );
+        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+    }
+  }
+}
+
+bool rgb_matrix_indicators_user(void) {
+  if (keyboard_config.disable_layer_led) { return false; }
+  switch (biton32(layer_state)) {
+    case DEF:
+    case SYM:
+    case NAV:
+    case NUM:
+      set_layer_color(biton32(layer_state));
+      break;
+   default:
+    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+      rgb_matrix_set_color_all(0, 0, 0);
+    break;
+  }
+  return true;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Oneshot mod implementation
+////////////////////////////////////////////////////////////////////////////////
+
+// Represents the four states a oneshot key can be in
+typedef enum {
+    os_down_unused, // Key is down and not used
+    os_down_used, // Key is down and used
+    os_up_unqueued, // Key is up and not queued
+    os_up_queued,  // Key is up and queued
+} oneshot_state;
+
+// To be implemented by the consumer. Defines keys to cancel oneshot mods.
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    switch (keycode) {
+    case KC_ESC:
+        return true;
+    case LA_SYM:
+    case LA_NAV:
+    default:
+        return false;
+    }
+}
+
+// To be implemented by the consumer. Defines keys to ignore when determining
+// whether a oneshot mod has been used. Setting this to modifiers and layer
+// change keys allows stacking multiple oneshot modifiers, and carrying them
+// between layers.
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    switch (keycode) {
+    case KC_ESC:
+    case LA_SYM:
+    case LA_NAV:
+    case KC_LSFT:
+    case OS_SHFT:
+    case OS_CTRL:
+    case OS_ALT:
+    case OS_CMD:
+    case OS_HYPR:
+        return true;
+    default:
+        return false;
+    }
+}
+
+// Custom oneshot mod implementation that doesn't rely on timers. If a mod is
+// used while it is held it will be unregistered on keyup as normal, otherwise
+// it will be queued and only released after the next non-mod keyup.
+void update_oneshot(
+    oneshot_state *state,
+    uint16_t mod,
+    uint16_t trigger,
+    uint16_t keycode,
+    keyrecord_t *record
+) {
+    bool key_down = record->event.pressed;
+    if (keycode == trigger) {
+        if (key_down) {
+            // Trigger keydown
+            if (*state == os_up_unqueued) {
+                register_code(mod);
+            }
+            *state = os_down_unused;
+        } else {
+            // Trigger keyup
+            switch (*state) {
+            case os_down_unused:
+                // If we didn't use the mod while trigger was held, queue it.
+                *state = os_up_queued;
+                break;
+            case os_down_used:
+                // If we did use the mod while trigger was held, unregister it.
+                *state = os_up_unqueued;
+                unregister_code(mod);
+                break;
+            default:
+                break;
+            }
+        }
+    } else {
+        if (key_down) {
+            if (is_oneshot_cancel_key(keycode) && *state != os_up_unqueued) {
+                // Cancel oneshot on designated cancel keydown.
+                *state = os_up_unqueued;
+                unregister_code(mod);
+            }
+        } else {
+            if (!is_oneshot_ignored_key(keycode)) {
+                // On non-ignored keyup, consider the oneshot used.
+                switch (*state) {
+                case os_down_unused:
+                    *state = os_down_used;
+                    break;
+                case os_up_queued:
+                    *state = os_up_unqueued;
+                    unregister_code(mod);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
+
+bool sw_win_active = false;
+
+oneshot_state os_shft_state = os_up_unqueued;
+oneshot_state os_ctrl_state = os_up_unqueued;
+oneshot_state os_alt_state = os_up_unqueued;
+oneshot_state os_cmd_state = os_up_unqueued;
+oneshot_state os_hypr_state = os_up_unqueued;
+
+void update_swapper(
+    bool *active,
+    uint16_t cmdish,
+    uint16_t tabish,
+    uint16_t trigger,
+    uint16_t keycode,
+    keyrecord_t *record
+) {
+    if (keycode == trigger) {
+        if (record->event.pressed) {
+            if (!*active) {
+                *active = true;
+                register_code(cmdish);
+            }
+            register_code(tabish);
+        } else {
+            unregister_code(tabish);
+            // Don't unregister cmdish until some other key is hit or released.
+        }
+    } else if (*active) {
+        unregister_code(cmdish);
+        *active = false;
+    }
+}
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    update_swapper(&sw_win_active, KC_LGUI, KC_TAB, SW_WIN, keycode, record);
+
+    update_oneshot(&os_shft_state, KC_LSFT, OS_SHFT, keycode, record);
+    update_oneshot(&os_shft_state, KC_LSFT, OS_HYPR, keycode, record);
+    update_oneshot(&os_shft_state, KC_LSFT, OS_MEH, keycode, record);
+
+    update_oneshot(&os_ctrl_state, KC_LCTL, OS_CTRL, keycode, record);
+    update_oneshot(&os_ctrl_state, KC_LCTL, OS_HYPR, keycode, record);
+    update_oneshot(&os_ctrl_state, KC_LCTL, OS_MEH, keycode, record);
+
+    update_oneshot(&os_alt_state, KC_LALT, OS_ALT, keycode, record);
+    update_oneshot(&os_alt_state, KC_LALT, OS_HYPR, keycode, record);
+    update_oneshot(&os_alt_state, KC_LALT, OS_MEH, keycode, record);
+
+    update_oneshot(&os_cmd_state, KC_LCMD, OS_CMD, keycode, record);
+    update_oneshot(&os_cmd_state, KC_LCMD, OS_HYPR, keycode, record);
+
+    return true;
+}
+
+//uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+//    switch (keycode) {
+//        case OSL(1):
+//            return 1250;
+//        default:
+//            return TAPPING_TERM;
+//    }
+//}
+//
+layer_state_t layer_state_set_user(layer_state_t state) {
+    uint8_t layer = biton(state);
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+  switch (layer) {
+    case 1:
+      ergodox_right_led_1_on();
+      break;
+    case 2:
+      ergodox_right_led_2_on();
+      break;
+    case 3:
+      ergodox_right_led_3_on();
+      break;
+    case 4:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      break;
+    case 5:
+      ergodox_right_led_1_on();
+      ergodox_right_led_3_on();
+      break;
+    case 6:
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      break;
+    case 7:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_on();
+      break;
+    default:
+      break;
+  }
+    return update_tri_layer_state(state, SYM, NAV, NUM);
+}
