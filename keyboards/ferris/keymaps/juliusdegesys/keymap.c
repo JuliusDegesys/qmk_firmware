8aa0fe148c1cb96791e1853073995d38bae0ed19
Julius Degesys <julius@moonlite.ai>
2025-10-06 13:27:45 -0400

    Added HOME and END. Got rid of ESC on the top right.

--- keyboards/ferris/keymaps/juliusdegesys/keymap.c
+++ keyboards/ferris/keymaps/juliusdegesys/keymap.c
@@ -48,20 +48,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [SYM] = LAYOUT(
     KC_TILD, KC_QUOT, KC_DQUO, KC_AMPR, KC_HASH,    KC_PLUS, KC_EXLM, KC_QUES, KC_AT  , KC_ASTR,
-    KC_PIPE, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT,      KC_GT  , KC_RPRN, KC_RBRC, KC_RCBR, KC_ESC ,
+    KC_PIPE, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT,      KC_GT  , KC_RPRN, KC_RBRC, KC_RCBR, XXXXXXX,
     KC_GRV , KC_UNDS, KC_COLN, KC_BSLS, KC_EQL ,    KC_MINS, KC_PERC, KC_CIRC, KC_DLR , KC_SLSH,
                                _______, _______,    _______, _______
   ),
   [NAV] = LAYOUT(
-    CMD_GRV, CMD_TAB, LGUI(LSFT(KC_T)), LGUI(KC_T), LGUI(KC_SPACE)  ,    KC_PGUP, CTRL_SHIFT_TAB, KC_UP  , CTRL_TAB, KC_ESC ,
+    CMD_GRV, CMD_TAB, LGUI(LSFT(KC_T)), LGUI(KC_T), LGUI(KC_SPACE)  ,    KC_PGUP, CTRL_SHIFT_TAB, KC_UP  , CTRL_TAB, XXXXXXX,
     OS_SHFT, OS_CTRL, OS_ALT          , OS_CMD    , OS_HYPR         ,    KC_ENT , KC_LEFT       , KC_DOWN, KC_RGHT , KC_BSPC,
-    KC_UNDO,  KC_CUT, KC_COPY         , KC_PASTE  , LGUI(LSFT(KC_4)),    KC_PGDN, LSFT(KC_TAB)  , KC_TAB , _______ , KC_DEL ,
+    KC_UNDO,  KC_CUT, KC_COPY         , KC_PASTE  , LGUI(LSFT(KC_4)),    KC_PGDN, LSFT(KC_TAB)  , KC_TAB , XXXXXXX ,  KC_DEL,
                                         _______   , _______         ,    _______, _______
   ),
   [NUM] = LAYOUT(
-    KC_F8  , KC_F7 , KC_F6 , KC_F5, _______,    KC_PLUS, KC_7, KC_8, KC_9, KC_ASTR,
-    KC_F4  , KC_F3 , KC_F2 , KC_F1, _______,    KC_0   , KC_4, KC_5, KC_6, KC_BSPC,
-    KC_F12,  KC_F11, KC_F10, KC_F9, _______,    KC_MINS, KC_1, KC_2, KC_3, KC_SLSH,
+    KC_F8  , KC_F7 , KC_F6 , KC_F5, KC_HOME,    KC_PLUS, KC_7, KC_8, KC_9, KC_ASTR,
+    KC_F4  , KC_F3 , KC_F2 , KC_F1, KC_END ,    KC_0   , KC_4, KC_5, KC_6, KC_BSPC,
+    KC_F12,  KC_F11, KC_F10, KC_F9, XXXXXXX,    KC_MINS, KC_1, KC_2, KC_3, KC_SLSH,
                            _______, _______,    QK_BOOT, _______
   ),
 };
